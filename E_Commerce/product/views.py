6ab1c71c571499f925454e924f552524862f8590
sahil-verma-818 <sahil.v@cisinlabs.com>
2023-11-06 19:00:22 +0530

    Everything going fine till now. Only setting checkpoints

--- E_Commerce/product/views.py
+++ E_Commerce/product/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
 
 def product_category(request, id):
+
+    data = Product.objects.get(id=id)
+    context = {
+        'data' : data
+    }
     
-    return render(request, 'product_template/index.html')
+    return render(request, 'product_template/detail.html', context)
 
 def wishlist(request):
 
