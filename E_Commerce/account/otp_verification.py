e139ef8595a9f18375b24d8520eb60702280a5ae
sahil-verma-818 <sahil.v@cisinlabs.com>
2024-01-12 20:50:21 +0530

    Updated countdown

--- E_Commerce/account/otp_verification.py
+++ E_Commerce/account/otp_verification.py
@@ -1,12 +1,14 @@
 import random
 from django.conf import settings
 from twilio.rest import Client
+from datetime import datetime, timedelta
 
 
 class OneTimePassword:
 
     def get_otp(self):
         OneTimePassword.otp_input = str(random.randint(100000,999999))
+        OneTimePassword.exp_time = datetime.now() + timedelta(minutes=3)
         print("Generated OTP : ", self.otp_input)
         client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
         # message = client.messages.create(
@@ -16,8 +18,10 @@ class OneTimePassword:
         #     )
 
     def validate_otp(otp,request):
-        if OneTimePassword.otp_input == request.POST.get('otp'):
-            print("Otp validation successful")
-            return True
+        if datetime.now() < OneTimePassword.exp_time:
+
+            if OneTimePassword.otp_input == request.POST.get('otp'):
+                print("Otp validation successful")
+                return True
 
     
