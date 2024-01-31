0117565175225bbc74da4761ad5734f82c5136c1
sahil-verma-818 <sahil.v@cisinlabs.com>
2024-01-31 13:30:35 +0530

    Changes mostly as migrations

--- E_Commerce/E_Commerce/settings.py
+++ E_Commerce/E_Commerce/settings.py
@@ -89,6 +89,19 @@ DATABASES = {
 }
 
 
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'E-Commerce',
+#         'USER': 'root',
+#         'PASSWORD': '',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306'
+#     }
+# }
+
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
